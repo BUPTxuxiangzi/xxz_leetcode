42905ed9d28f1a9d740744ba7714e86abcf39d70
XuXiangzi <1499734120@qq.com>
2015-03-31 20:35:57 +0800

    leetcode66

---
+++ leetcode_66_PlusOne.cpp
@@ -0,0 +1,10 @@
+/*问题描述
+Plus One
+Total Accepted: 41324 Total Submissions: 134070
+
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+数组的低位存储数字的高位
+*/
+
