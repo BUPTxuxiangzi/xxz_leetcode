9e58ebea4477c5497e682199e9691025088e417c
XuXiangzi <1499734120@qq.com>
2015-04-01 18:55:32 +0800

    leetcode66

--- leetcode_66_PlusOne.cpp
+++ leetcode_66_PlusOne.cpp
@@ -8,3 +8,25 @@ The digits are stored such that the most significant digit is at the head of the
 数组的低位存储数字的高位
 */
 
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+    int carry = 1;
+	vector<int>::iterator iter = digits.end();
+	int tmp = 0;
+	do
+	{
+		iter--;
+		tmp = *iter;
+		(*iter) = (tmp + carry) % 10;
+		carry = ( tmp + carry) / 10;
+	
+	} while (iter != digits.begin() );
+
+	if (carry) //carry != 0
+	{
+		digits.insert(digits.begin(), carry);
+	}
+	return digits;
+    }
+};
