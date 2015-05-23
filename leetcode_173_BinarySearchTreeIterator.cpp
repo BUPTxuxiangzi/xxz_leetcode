f6f30924ff3b79c1426a3d6857ed18c4d048e705
XuXiangzi <1499734120@qq.com>
2015-05-23 20:45:00 +0800

    leetcode173

---
+++ leetcode_173_BinarySearchTreeIterator.cpp
@@ -0,0 +1,56 @@
+/*
+Binary Search Tree Iterator 
+
+Implement an iterator over a binary search tree (BST). Your iterator will be 
+initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, 
+where h is the height of the tree. 
+*/
+
+/*解法1：
+Status: Accepted
+Runtime: 28 ms
+
+*/
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root)
+    {
+        while (!sta.empty()) //将栈清空
+        {
+            sta.pop();
+        }
+
+        TreeNode *p = root;
+        while (p != nullptr) {
+            sta.push(p);
+            p = p->left;
+        } //一路向左，直到最后
+    }
+
+    bool hasNext()
+    {
+        return !sta.empty();
+    }
+
+    int next()
+    {
+        TreeNode *p = sta.top();
+        int result = p->val;
+        sta.pop();
+        p = p->right;
+        while (p != nullptr) {
+            sta.push(p);
+            p = p->right;
+        }
+
+        return result;
+    }
+
+private:
+    stack<TreeNode *> sta;
+};
